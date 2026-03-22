57d10a1bf9362fce321fa0793b09017c2ca0d200
Jonathan Chimino <31198118+jchimino@users.noreply.github.com>
2026-03-22 14:51:10 -0400

    Add assess_health router to API

--- python_app/routers/api/__init__.py
+++ python_app/routers/api/__init__.py
@@ -2,11 +2,12 @@
 
 from fastapi import APIRouter
 
-from . import verify_label, scan_file, applications, history
+from . import verify_label, scan_file, applications, history, assess_health
 
 router = APIRouter(prefix="/api")
 
 # Include sub-routers
+router.include_router(assess_health.router)
 router.include_router(verify_label.router)
 router.include_router(scan_file.router)
 router.include_router(applications.router)
